c179aa3ee6604287743ca853e2016f6ae96e9b4d
phiquan123qa <phiquan070902@gmail.com>
2024-01-21 23:44:11 +0700

    test

---
+++ src/main/java/com/vn/realestatewebsiteproject/RealEstateWebsiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vn.realestatewebsiteproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateWebsiteProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateWebsiteProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vn/realestatewebsiteproject/RealEstateWebsiteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.realestatewebsiteproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateWebsiteProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
