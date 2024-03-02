b74fde3c55d268ab8ef9c60722cdeab7570fae3f
Nguyên Phi Quân <phiquan070902@gmail.com>
2024-03-02 13:41:51 +0700

    jhjh

--- src/main/java/com/vn/RealEstateWebsiteProjectApplication.java
+++ src/main/java/com/vn/RealEstateWebsiteProjectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RealEstateWebsiteProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RealEstateWebsiteProjectApplication.class, args);
     }
-
 }
